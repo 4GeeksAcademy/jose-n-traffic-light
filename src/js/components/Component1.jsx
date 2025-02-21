8f63b36b26dd7e926525f1d6a0d299a6e7c931f7
jlnunezg10 <juniorng10@gmail.com>
2025-02-21 01:39:42 +0000

    First Commit

---
+++ src/js/components/Component1.jsx
@@ -0,0 +1,56 @@
+import React, {useEffect, useState} from 'react'
+
+const Component1 = () => {
+
+    const [selectRed,setSelectRed] = useState(false)
+    const [selectYellow,setSelectYellow] = useState(false)
+    const [selectGreen,setSelectGreen] = useState(false)
+
+    const handlerRed = () =>{
+        setSelectRed(true);
+        setSelectYellow(false)
+        setSelectGreen(false)
+    }
+
+    const handlerYellow = () =>{
+        setSelectRed(false);
+        setSelectYellow(true)
+        setSelectGreen(false)
+    }
+
+    const handlerGreen = () =>{
+        setSelectRed(false);
+        setSelectYellow(false)
+        setSelectGreen(true)
+    }
+
+
+
+  return (
+    <div className='container d-flex flex-column box'>
+
+        <div className='row d-flex justify-content-center' onClick={handlerRed}>
+
+
+            <div className={selectRed ? 'red-light selected': 'red-light' } ></div>
+            
+            
+        </div>
+
+        <div className='row d-flex justify-content-center' onClick={handlerYellow}>
+        <div className={selectYellow? 'yellow-light selected': 'yellow-light' } ></div>
+        </div>
+
+
+        <div className='row d-flex justify-content-center' onClick={handlerGreen}>
+        <div className={selectGreen? 'green-light selected': 'green-light' } ></div>
+        </div>
+
+        
+
+
+    </div>
+  )
+}
+
+export default Component1
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Component1 from './components/Component1';
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
+
+
 
 // index.css'
 import '../styles/index.css'
@@ -13,6 +16,8 @@ import Home from './components/Home';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    {/* <Home/> */}
+    <Component1/>
+    
   </React.StrictMode>,
 )
